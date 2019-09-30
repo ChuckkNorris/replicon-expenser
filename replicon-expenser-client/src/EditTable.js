cb371f7151fbd5099c14556f8fb345e307652e68
Muizz Soomar <muizzsoomar@gmail.com>
2019-09-30 13:38:24 -0500

    Fixed button

--- replicon-expenser-client/src/EditTable.js
+++ replicon-expenser-client/src/EditTable.js
@@ -68,7 +68,7 @@ export default ({ data, header }) => {
           {storedFiles.map((file, index) => (
             <TableRow>
               <TableCell align="right">{file.NameParts[index]}</TableCell>
-              {/* <TableCell align="right">{file.NameParts[1]}</TableCell>
+              <TableCell align="right">{file.NameParts[1]}</TableCell>
               <TableCell align="right">{file.NameParts[2]}</TableCell>
               <TableCell align="right">{file.NameParts[3]}</TableCell>
               <TableCell align="right">
@@ -76,7 +76,7 @@ export default ({ data, header }) => {
               </TableCell>
               <TableCell>
                 <FilePreview fileLink={file.Link} fileName={file.Name} />
-              </TableCell> */}
+              </TableCell>
               <TableCell>
                                                                        
                 <EditIcon />
@@ -88,7 +88,6 @@ export default ({ data, header }) => {
               </TableCell>
             </TableRow>
           ))}
-          {/* </TableBody> */}
         </Table>
       </Paper>
     </div>
--- replicon-expenser-client/src/ReceiptUpload.js
+++ replicon-expenser-client/src/ReceiptUpload.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { DropzoneArea } from "material-ui-dropzone";
 
 export default function UploadReceipts(props) {
@@ -20,14 +20,11 @@ export default function UploadReceipts(props) {
 
   return (
     <div>
-      <button type="button" onClick={handleOpen}>
-        Upload Receipts
-      </button>
       <DropzoneArea
         open={open}
         onChange={handleSave}
         acceptedFiles={["image/jpeg", "image/png", "image/bmp"]}
-        showPreviewsInDropzone={false}        
+        showPreviewsInDropzone={false}
         maxFileSize={5000000}
         onClose={handleClose}
         filesLimit={100}
